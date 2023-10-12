ae0f13ec7ca377aa5375bbe11a085d430f5647df
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2023-10-13 00:34:44 +0400

    + enums

---
+++ src/main/kotlin/model/enums/Command.kt
@@ -0,0 +1,7 @@
+package model.enums
+
+enum class Command(val cmd: String) {
+    START("/start"),
+    REGISTRATION("/registration"),
+    HELP("/help"),
+}
---
+++ src/main/kotlin/model/enums/Error.kt
@@ -0,0 +1,5 @@
+package model.enums
+
+enum class Error(val message: String) {
+    UNEXPECTED_EVENT("Что-то пошло не так.\nПопробуй сначала, отправив нужную команду.")
+}
